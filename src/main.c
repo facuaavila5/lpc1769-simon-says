6da504fbcb9f32ff5047706414aa4b503239bc73
facuaavila5 <facundo.avila.027@mi.unc.edu.ar>
2025-10-16 22:40:28 -0300

    First commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include "lpc17xx_gpdma.h"
+#include "lpc17xx_timer.h"
+#include "lpc17xx_adc.h"
+#include "lpc17xx_dac.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_eint.h"
+
+int main() {
+
+    while(1) {
+
+    }
+    return 0;
+}
