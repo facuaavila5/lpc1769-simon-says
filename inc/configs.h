bcd9a332ba94998142b995bd60ceeb9e74dda407
facuaavila5 <facundo.avila.027@mi.unc.edu.ar>
2025-10-17 02:45:55 -0300

    Add configs source and header files

---
+++ inc/configs.h
@@ -0,0 +1,13 @@
+#ifndef CONFIGS_H
+#define CONFIGS_H
+
+// function prototypes
+void config_pcb(void);
+void config_adc(void);
+void config_dma(void);
+void config_dac(void);
+void config_eint(void);
+void config_timer_0(void);
+void config_timer_1(void);
+
+#endif
---
+++ src/configs.c
@@ -0,0 +1,29 @@
+#include "../inc/configs.h"
+
+void config_pcb(void) {
+
+}
+
+void config_adc(void) {
+
+}
+
+void config_dma(void) {
+
+}
+
+void config_dac(void) {
+
+}
+
+void config_eint(void) {
+
+}
+
+void config_timer_0(void) {
+
+}
+
+void config_timer_1(void) {
+    
+}
