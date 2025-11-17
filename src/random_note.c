811c24b9b1594c9bdce62180d67b14dbb948065e
facuaavila5 <facundo.avila.027@mi.unc.edu.ar>
2025-11-17 15:29:56 -0300

    Removed unused files

--- src/random_note.c
+++
@@ -1,8 +0,0 @@
-#include "random_note.h"
-
-const uint16_t notes[] = {262, 294, 330, 349, 392, 440, 494, 523};
-
-void generate_note(void) {
-    uint16_t *note_ptr = (uint16_t *)NOTE_FREQ_ADDRESS;
-    *note_ptr = 440; // hardcode
-}
